b3e071d13c0a23c055d0057635065eb7229820ea
Pablo Cantero <pablo@pablocantero.com>
2014-02-07 20:01:16 -0200

    [WIP] Graphite

--- lib/mongo_profiler/web_helpers.rb
+++ lib/mongo_profiler/web_helpers.rb
@@ -3,5 +3,18 @@ module MongoProfiler
     def root_path
       "#{env['SCRIPT_NAME']}/"
     end
+
+    def graphite_graph_url(profile, from, size)
+      file   = sanitaze_stat_key(profile['file'].split('/').last)
+      method = sanitaze_stat_key(profile['method'])
+
+      "http://graphite.spree.fm/render?from=#{from}&until=now&width=400&height=250&target=alias(summarize(stats_counts.augury.staging.mongo_profiler.#{MongoProfiler.application_name}.#{file}.#{method}, '#{size}', 'sum'), '#{file}#{method}')&title=Last day/hour"
+    end
+
+    private
+
+    def sanitaze_stat_key(key)
+      key.gsub(/\W/, '_')
+    end
   end
 end
---
+++ spec/mongo_profiler/web_helpers_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+require 'mongo_profiler/web_helpers'
+
+module MongoProfiler
+  describe WebHelpers do
+  end
+end
